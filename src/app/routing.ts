957e936e3406f85163a537a3ede4a06dbd0ce24f
asmitapaul22 <asmitapaul2000@gmail.com>
2020-08-25 11:28:07 +0530

    first commit

---
+++ src/app/routing.ts
@@ -0,0 +1,10 @@
+ import {Routes} from '@angular/router';
+ import {AdminComponent} from './admin/admin.component';
+ import {HomeComponent} from './home/home.component';
+ import {ActivateGuard} from './activate.guard';
+import { Component } from '@angular/core';
+ export class Approutes:Routes=
+ {
+     {path:'', Component: HomeComponent},
+
+ } 
